d22082872edc0ff2ae6654b5a51171fe0eb0a6b2
Nkiruka Ebele <specialebele@gmail.com>
2022-03-10 16:03:31 +0000

    Updated file 6-sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,10 +16,10 @@ int j;
 long int k;
 long long int l;
 float m;
-printf("Size of a char: %zu bytes(s)\n", sizeof(i));
-printf("Size of a int: %zu bytes(s)\n", sizeof(j));
-printf("Size of a long long: %zu bytes(s)\n", sizeof(k));
-printf("Size of a long long int: %zu bytes(s)\n", sizeof(l));
-printf("Size of a float: %zu bytes(s)\n", sizeof(m));
+printf("Size of a char: %zu byte(s)\n", sizeof(i));
+printf("Size of a int: %zu byte(s)\n", sizeof(j));
+printf("Size of a long long: %zu byte(s)\n", sizeof(k));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(l));
+printf("Size of a float: %zu byte(s)\n", sizeof(m));
 return (0);
 }
