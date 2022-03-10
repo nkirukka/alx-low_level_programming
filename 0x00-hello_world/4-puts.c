7eb4afc34cb01924d407551b40fa5bea6dc5ba2e
Nkiruka Ebele <specialebele@gmail.com>
2022-03-10 15:10:41 +0000

    Added more comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-/* main is the entry point */
+/* Main - Entry point
+ *
+ * Description: prints "Programming is like building a multilingual puzzle"
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
