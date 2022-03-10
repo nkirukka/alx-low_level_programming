cbebf3045a32c354bcf7db040692941caaece70f
Nkiruka Ebele <specialebele@gmail.com>
2022-03-10 16:58:36 +0000

    Update file 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/** 
+/**
  * main - print quote
  *
  * Description: prints Dora's quote
@@ -11,6 +11,6 @@
 
 int main(void)
 {
-write(2 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-return(1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
