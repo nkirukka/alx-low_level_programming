433ac8a1b58d8b5b39892d99fe875079e3f0f3f4
Nkiruka Ebele <specialebele@gmail.com>
2022-03-11 15:21:20 +0000

    Print numbers with char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print base 10 numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 48; i < 58; i++)
+putchar(i);
+putchar(10);
+return (0);
+}
+
