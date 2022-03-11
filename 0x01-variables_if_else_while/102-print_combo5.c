ccf925eb19da754cf4bff53ea4ec6c77d9e231ca
Nkiruka Ebele <specialebele@gmail.com>
2022-03-11 16:30:46 +0000

    Add new files

--- 0x01-variables_if_else_while/102-print_combo5.c
+++ 0x01-variables_if_else_while/102-print_combo5.c
@@ -1,47 +1,48 @@
 #include <stdio.h>
 
 /**
- * main - print triple combos
+ * main - print pairs of double digit combos
  *
- * Description: print triple digit combos
+ * Description: print pairs of double digit combos
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-int i, j, k;
+int i, j, k, m;
 
 i = 48;
-j = 48;
-k = 48;
-
 while (i < 58)
 {
-j = i + 1;
+j = 48;
 while (j < 58)
 {
-k = j + 1;
+m = j + 1;
+k = i;
 while (k < 58)
+{
+while (m < 58)
 {
 putchar(i);
 putchar(j);
+putchar(32);
 putchar(k);
-if (i < 55 || j < 56 || k < 57)
+putchar(m);
+if (i < 57 || j < 56 || k < 57 || m < 57)
 {
 putchar(44);
 putchar(32);
 }
+m++;
+}
+m = 48;
 k++;
 }
-
 j++;
 }
-
 i++;
 }
-
 putchar(10);
-
 return (0);
 }
