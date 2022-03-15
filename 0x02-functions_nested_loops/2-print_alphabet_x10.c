93b012c21776e4f31cfeda7f1326e9d520b3dfbc
Nkiruka Ebele <specialebele@gmail.com>
2022-03-15 12:18:45 +0000

    Refactor

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,15 +4,18 @@
 /**
  * print_alphabet - print alphabet ten times
  *
+ * Description: print alpha ten times
+ *
  * Return: void
  */
 
 void print_alphabet_x10(void)
 {
 int i = 10;
+int j = 97;
 while (i < 10)
 {
-int j = 97;
+j = 97;
 while (j < 123)
 {
 _putchar(j);
