d5c8cae7be6a0a4130b89cf6d60cd3c6ab570a85
nkirukka <72361299+nkirukka@users.noreply.github.com>
2022-03-21 22:54:19 +0100

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,12 +3,12 @@
 
 void puts2(char *s)
 {
-	int i;
-	for (i = 0; s[i] != '\0'; i++)
-    {
-        if (i % 2 == 0){
-            _putchar(s[i]);
-        }
-    }
-	_putchar('\n');
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (i % 2 == 0){
+_putchar(s[i]);
+}
+}
+_putchar('\n');
 }
