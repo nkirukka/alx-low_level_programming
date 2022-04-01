f2f928cd454686b02e48aa2350fcc374f5fa2f88
Nkiruka Ebele <specialebele@gmail.com>
2022-04-01 13:31:03 +0000

    Add files: readme.md, main.h, 0-whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - print name of program
+ *
+ * @argc: argument count
+ *
+ * @argv: argument vector
+ *
+ * Return: Always 0(Success)
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
