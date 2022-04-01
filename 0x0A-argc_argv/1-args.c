50271e8becdbdab6a7792b09b79681fa11ec3529
Nkiruka Ebele <specialebele@gmail.com>
2022-04-01 13:52:06 +0000

    Update file 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,8 +1,20 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * main - print number of arguments
+ *
+ * @argc: arg count
+ *
+ * @argv: arg vector
+ *
+ * Return: Always 0(Success)
+ */
+
+
+
 int main(__attribute__((unused)) int argc, char *argv[])
 {
-printf("%d\n", argc-1);
-return(0);
+printf("%d\n", argc - 1);
+return (0);
 }
