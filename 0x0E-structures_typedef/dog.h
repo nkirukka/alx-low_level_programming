c0c986af08a63f0426f86e43cb9cf187194dcfd8
Nkiruka Ebele <specialebele@gmail.com>
2022-04-11 21:54:04 +0000

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct Dog - define a new type struct
+ *
+ * @name: type of char
+ * @age: type of float
+ * @owner: type of char
+ *
+ * Description: define a type of struct with the above members
+ */
+
+
+
+struct Dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif /* DOG_H */
