f2a0c21b768a47551407d83659d29ab42d2d4606
Nkiruka Ebele <specialebele@gmail.com>
2022-04-11 22:15:52 +0000

    Create file 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize a type of struct dog
+ * @d: struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d != NULL)
+    {
+        d->name = name;
+        d->age = age;
+        d->owner = owner;
+    }
+}
