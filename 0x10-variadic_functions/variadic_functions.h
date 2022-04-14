69f57f64d9a68cb68c61d47af5dea1f3176a98f8
nkirukka <72361299+nkirukka@users.noreply.github.com>
2022-04-14 17:26:16 +0100

    Update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,4 +8,20 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+
+/**
+ * struct op - Struct op
+ *
+ * @c: The data type (char)
+ * @f: The function associated with it
+ */
+
+typedef struct op
+{
+	char c;
+	void (*f)(va_list);
+} op_t;
+
+void print_all(const char * const format, ...);
+
 #endif
